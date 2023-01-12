6aa373b5232061497d5f3005f168cf51632e03e5
yoon3nam <lgmysn2@gmail.com>
2023-01-12 17:25:22 +0900

    바닐라 자바스크립트 (리덕스 적용전)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import { legacy_createStore as createStore } from "redux";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+let count = 0;
+
+number.innerText = count;
+
+const updateText = () => {
+  number.innerText = count;
+};
+const handleAdd = () => {
+  count = count + 1;
+  updateText();
+};
+const handleMinus = () => {
+  count = count - 1;
+  updateText();
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
