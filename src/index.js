21cc92579230687d6653e41303edf071fb766b54
yoon3nam <lgmysn2@gmail.com>
2023-01-12 18:32:42 +0900

    리덕스를 적용한 바닐자 자바스크립트의 카운터

--- src/index.js
+++ src/index.js
@@ -4,21 +4,29 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
+//액션을 단순한 문자열이 아닌 아래와 같이 변수에 할당 해주면 오류를 잡아내기에 훨씬 용이하다. (안전성)
+const ADD = "ADD";
+const MINUS = "MINUS";
 
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
-};
-const handleAdd = () => {
-  count = count + 1;
-  updateText();
-};
-const handleMinus = () => {
-  count = count - 1;
-  updateText();
+//리듀서에는 if문 보다 switch문을 쓰는 것이 조금 더 간결하고 깔끔한 코딩이 가능하다.
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
 };
 
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+const countStore = createStore(countModifier);
+number.innerText = countStore.getState();
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+add.addEventListener("click", () => countStore.dispatch({ type: ADD }));
+minus.addEventListener("click", () => countStore.dispatch({ type: MINUS }));
