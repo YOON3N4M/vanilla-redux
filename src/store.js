7c6a3c30516810c65ef2a49ee2ddb366bacc7d4f
yoon3nam <lgmysn2@gmail.com>
2023-01-13 19:08:13 +0900

    4.1 createAction 까지 완료한 상태 action의 코드들을 createAction을 통해 변경하였음

--- src/store.js
+++ src/store.js
@@ -1,11 +1,11 @@
 import { legacy_createStore as createStore } from "redux";
-
-//액션 타입
-const ADD = "ADD";
+import { createAction } from "@reduxjs/toolkit";
+//액션 타입( redux-toolkit 적용전)
+/*const ADD = "ADD";
 const DELETE = "DELETE";
-
-//액션 크리에이터
-const addTodo = (text) => {
+*/
+//액션 크리에이터 (redux-toolkit 적용전)
+/* const addTodo = (text) => {
   return {
     type: ADD,
     text,
@@ -18,14 +18,20 @@ const deleteTodo = (id) => {
     id: parseInt(id),
   };
 };
+*/
+
+//액션 크리에이터, 액션 타입(redux-toolkit 적용후)
+
+const addTodo = createAction("ADD");
+const deleteTodo = createAction("DELETE");
 
 //리듀서
 const reducer = (state = [], action) => {
   switch (action.type) {
-    case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE:
-      return state.filter((toDo) => toDo.id !== action.id);
+    case addTodo.type:
+      return [{ text: action.payload, id: Date.now() }, ...state];
+    case deleteTodo.type:
+      return state.filter((toDo) => toDo.id !== action.payload);
     default:
       return state;
   }
